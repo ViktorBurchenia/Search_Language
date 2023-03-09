27b6d07ff586207899eb059c509b4c4a8b08b0ad
ViktorBurchenia <viktorpux2@gmail.com>
2023-03-09 23:39:52 +0200

    Fixed (refactoring code, fix seed and rubocop)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+require 'json'
+
+data = JSON.parse(File.read('./db/data.json'))
+
+data.each do |language_data|
+  Language.create(name: language_data['name'], language_type: language_data['type'].gsub(/, /, ','),
+                  designed_by: language_data['designed by'].gsub(/, /, ','))
+end
